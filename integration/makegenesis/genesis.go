a888ef3994ab25dd265eab50dd50d9dc487ca362
loganworld <akp707d@gmail.com>
2022-09-22 13:06:32 +0800

    first commit

---
+++ integration/makegenesis/genesis.go
@@ -0,0 +1,493 @@
+package makegenesis
+
+import (
+	"crypto/ecdsa"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"math/rand"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/galaxy-digital/lachesis-base/hash"
+	"github.com/galaxy-digital/lachesis-base/inter/idx"
+
+	galaxy "github.com/galaxy-digital/galaxychain-chain/galaxy"
+	"github.com/galaxy-digital/galaxychain-chain/galaxy/genesis"
+	"github.com/galaxy-digital/galaxychain-chain/galaxy/genesis/driver"
+	"github.com/galaxy-digital/galaxychain-chain/galaxy/genesis/driverauth"
+	"github.com/galaxy-digital/galaxychain-chain/galaxy/genesis/evmwriter"
+	"github.com/galaxy-digital/galaxychain-chain/galaxy/genesis/gpos"
+	"github.com/galaxy-digital/galaxychain-chain/galaxy/genesis/netinit"
+	"github.com/galaxy-digital/galaxychain-chain/galaxy/genesis/sfc"
+	"github.com/galaxy-digital/galaxychain-chain/galaxy/genesisstore"
+	"github.com/galaxy-digital/galaxychain-chain/inter"
+	"github.com/galaxy-digital/galaxychain-chain/inter/validatorpk"
+	futils "github.com/galaxy-digital/galaxychain-chain/utils"
+)
+
+var (
+	FakeGenesisTime = inter.Timestamp(1608600000 * time.Second)
+)
+
+// FakeKey gets n-th fake private key.
+func FakeKey(n int) *ecdsa.PrivateKey {
+	reader := rand.New(rand.NewSource(int64(n)))
+
+	key, err := ecdsa.GenerateKey(crypto.S256(), reader)
+
+	fmt.Printf("\nYour new privatekey was generated %x\n", key.D)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return key
+}
+
+type ValidatorAccount struct {
+	address   string
+	validator string
+}
+
+func MakeGenesisStore() *genesisstore.Store {
+	genStore := genesisstore.NewMemStore()
+	genStore.SetRules(galaxy.MainNetRules())
+
+	var validatorAccounts = []ValidatorAccount{
+		// for mainnet
+		{
+			address:   "0xe716BBB8cBf4aDCdDD02E2aA6707BD96BAf96FC6",
+			validator: "04024a184d9de61463224785b1fb690e747443f0cf9911a83fe57ec6800b4859a612826288b1b1348ffdda609dfc0c0302b62f8632bb9b542605582f472ace443d",
+		},
+		{
+			address:   "0x3e38b4a12Ff835d31574889F9CEe7709D73fA508",
+			validator: "04e8ab9dfece3373a80415312b624dd5041b1f500c9b7211c09b54cee655b368ade2b327333a7c824920815ae22748d83e424efa3f2a9481ab4721c16680c62700",
+		},
+		{
+			address:   "0x9fE7B52fC9310B98d45d5a925Fa056B6323dC9e3",
+			validator: "04b13364effc09c7e3e1105920e8cefe96d7319bbddd6ca392706ca2940e137411d2dfd14194134950411b7df1ba562c3d0eefa77d6d1741a55764bf3ecec7665b",
+		},
+		{
+			address:   "0xC14F3b064c9e21612Be5102a65ec315Ff7ee86c7",
+			validator: "04aea03416e786e4910a156ed8f20c74f995f6616ea54a67059d57294ff8c04b981cf887e4793a5e2f066f9528f796ede9854ebe64becc1437e104705c6ca5a0f8",
+		},
+
+		{
+			address:   "0xAA0F9213BE71F4a7330A721DBa1853500Ff2f940",
+			validator: "0492051c3ee2f7afdabff519647c47157be34057faa39956e6f347e861fda784a2268097a2026f8e7d2df350682952791184b5e94d80c051038530238534951a32",
+		},
+		{
+			address:   "0x123d3656F34a7b869B8C2f886D7Ec6DC6F83C546",
+			validator: "043c1e0d35e772e76d6f693cfb53619a6ec1b19ff4def8e970c0c3c2dd0e792ae0a91a9cd18b9c417eefa3724d8e1c27def9a106fd62704cf382813ce6d971d924",
+		},
+		{
+			address:   "0xF81ab1Ecf06ce556D6449798Ae5195fdD83335Aa",
+			validator: "04baef11c035f71fdae54dabfdd72c2752cfeb53dfa0e74aac70832c02ee984d7999dbbfe61e495ef6f37f89985ca8fc696198837d2d88cc411f0ca061683cca98",
+		},
+		{
+			address:   "0x3B68e62614ed908D5759695E85605fA1F71C638F",
+			validator: "04957365047fc52dc52333307c3a524e388f4913edb27d50219321807c94f74c48d08c78f8b966e943cf926b87167681aed19b5d281d458a47a13fc4ca815359ca",
+		},
+	}
+
+	var initialAccounts = []string{
+		"0xAA80c682108F52D5E62450161484d3448349d1bc",
+	}
+	num := len(validatorAccounts)
+
+	_total := int(4e8)
+	_validator := 0
+	_staker := int(1e6)
+	_initial := (_total - (_validator+_staker)*num) / len(initialAccounts)
+
+	totalSupply := futils.ToGlxy(uint64(_total))
+	balance := futils.ToGlxy(uint64(_validator))
+	stake := futils.ToGlxy(uint64(_staker))
+	initialBalance := futils.ToGlxy(uint64(_initial))
+
+	validators := make(gpos.Validators, 0, num)
+
+	now := time.Now() // current local time
+	// sec := now.Unix()      // number of seconds since January 1, 1970 UTC
+	nsec := now.UnixNano()
+	time := inter.Timestamp(nsec)
+	for i := 1; i <= num; i++ {
+		addr := common.HexToAddress(validatorAccounts[i-1].address)
+		pubkeyraw := common.Hex2Bytes(validatorAccounts[i-1].validator)
+		// fmt.Printf("\n# addr %x pubkeyraw %s len %d\n", addr, hex.EncodeToString(pubkeyraw), len(pubkeyraw))
+		validatorID := idx.ValidatorID(i)
+		pubKey := validatorpk.PubKey{
+			Raw:  pubkeyraw,
+			Type: validatorpk.Types.Secp256k1,
+		}
+
+		validators = append(validators, gpos.Validator{
+			ID:               validatorID,
+			Address:          addr,
+			PubKey:           pubKey,
+			CreationTime:     time,
+			CreationEpoch:    0,
+			DeactivatedTime:  0,
+			DeactivatedEpoch: 0,
+			Status:           0,
+		})
+	}
+	for _, val := range initialAccounts {
+		genStore.SetEvmAccount(common.HexToAddress(val), genesis.Account{
+			Code:    []byte{},
+			Balance: initialBalance,
+			Nonce:   0,
+		})
+	}
+	for _, val := range validators {
+		genStore.SetEvmAccount(val.Address, genesis.Account{
+			Code:    []byte{},
+			Balance: balance,
+			Nonce:   0,
+		})
+		genStore.SetDelegation(val.Address, val.ID, genesis.Delegation{
+			Stake:              stake,
+			Rewards:            new(big.Int),
+			LockedStake:        new(big.Int),
+			LockupFromEpoch:    0,
+			LockupEndTime:      0,
+			LockupDuration:     0,
+			EarlyUnlockPenalty: new(big.Int),
+		})
+	}
+
+	var owner common.Address
+	if num != 0 {
+		owner = validators[0].Address
+	}
+
+	genStore.SetMetadata(genesisstore.Metadata{
+		Validators:    validators,
+		FirstEpoch:    2,
+		Time:          time,
+		PrevEpochTime: time - inter.Timestamp(time.Time().Hour()),
+		ExtraData:     []byte("galaxy"),
+		DriverOwner:   owner,
+		TotalSupply:   totalSupply,
+	})
+	genStore.SetBlock(0, genesis.Block{
+		Time:        time - inter.Timestamp(time.Time().Minute()),
+		Atropos:     hash.Event{},
+		Txs:         types.Transactions{},
+		InternalTxs: types.Transactions{},
+		Root:        hash.Hash{},
+		Receipts:    []*types.ReceiptForStorage{},
+	})
+	// pre deploy NetworkInitializer
+	genStore.SetEvmAccount(netinit.ContractAddress, genesis.Account{
+		Code:    netinit.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// pre deploy NodeDriver
+	genStore.SetEvmAccount(driver.ContractAddress, genesis.Account{
+		Code:    driver.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// pre deploy NodeDriverAuth
+	genStore.SetEvmAccount(driverauth.ContractAddress, genesis.Account{
+		Code:    driverauth.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// pre deploy SFC
+	genStore.SetEvmAccount(sfc.ContractAddress, genesis.Account{
+		Code:    sfc.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// set non-zero code for pre-compiled contracts
+	genStore.SetEvmAccount(evmwriter.ContractAddress, genesis.Account{
+		Code:    []byte{0},
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+
+	return genStore
+}
+
+func MakeTestnetGenesisStore() *genesisstore.Store {
+	genStore := genesisstore.NewMemStore()
+	genStore.SetRules(galaxy.TestNetRules())
+	var validatorAccounts = []ValidatorAccount{
+		{
+			address:   "0x8aeb60FC9A074AF4E67C4E6FE0E2e6D4e7b513E1",
+			validator: "04e3bddf65c63f75522df7ba0bd941ea2b0ed57289e75a257ddc7d067603e48c4cbc0b00cbceb6c029cd0e360df66f7131d3cde6602c15ff8ded0a5647f3be6613",
+		},
+		{
+			address:   "0xfB4d81A31BcBC5E2024f6c4247DD2Ce913bd7c95",
+			validator: "047b74786b91887b616ef2be951819dec873d574b3f9e7ebb440534673c10cbb24ed4274701d17d1930e12bbff3a4ac5db3bdccf1fad7e2431bc6b8cc753949351",
+		},
+		{
+			address:   "0x3AD4767f9d90fc43cAc7E338f439B70621B02c1d",
+			validator: "0423d37f22bc4375c2f64bfc7f55067f9155d8ea97599489b26c369d38a856d381830b0472deb0232e39e4bc14b63873f6a6c3a90110c3a397bbd75686227b4e95",
+		},
+	}
+	var initialAccounts = []string{
+		"0xAA80c682108F52D5E62450161484d3448349d1bc",
+	}
+
+	num := len(validatorAccounts)
+
+	_total := int(22e6)
+	_validator := 0
+	_staker := int(1 * 1e4)
+	_initial := (_total - (_validator+_staker)*num) / len(initialAccounts)
+
+	totalSupply := futils.ToGlxy(uint64(_total))
+	balance := futils.ToGlxy(uint64(_validator))
+	stake := futils.ToGlxy(uint64(_staker))
+	initialBalance := futils.ToGlxy(uint64(_initial))
+
+	validators := make(gpos.Validators, 0, num)
+
+	now := time.Now() // current local time
+	// sec := now.Unix()      // number of seconds since January 1, 1970 UTC
+	nsec := now.UnixNano()
+	time := inter.Timestamp(nsec)
+	for i := 1; i <= num; i++ {
+		addr := common.HexToAddress(validatorAccounts[i-1].address)
+		pubkeyraw := common.Hex2Bytes(validatorAccounts[i-1].validator)
+		fmt.Printf("\n# addr %x pubkeyraw %s len %d\n", addr, hex.EncodeToString(pubkeyraw), len(pubkeyraw))
+		validatorID := idx.ValidatorID(i)
+		pubKey := validatorpk.PubKey{
+			Raw:  pubkeyraw,
+			Type: validatorpk.Types.Secp256k1,
+		}
+
+		validators = append(validators, gpos.Validator{
+			ID:               validatorID,
+			Address:          addr,
+			PubKey:           pubKey,
+			CreationTime:     time,
+			CreationEpoch:    0,
+			DeactivatedTime:  0,
+			DeactivatedEpoch: 0,
+			Status:           0,
+		})
+	}
+
+	for _, val := range initialAccounts {
+		genStore.SetEvmAccount(common.HexToAddress(val), genesis.Account{
+			Code:    []byte{},
+			Balance: initialBalance,
+			Nonce:   0,
+		})
+	}
+
+	for _, val := range validators {
+		genStore.SetEvmAccount(val.Address, genesis.Account{
+			Code:    []byte{},
+			Balance: balance,
+			Nonce:   0,
+		})
+		genStore.SetDelegation(val.Address, val.ID, genesis.Delegation{
+			Stake:              stake,
+			Rewards:            new(big.Int),
+			LockedStake:        new(big.Int),
+			LockupFromEpoch:    0,
+			LockupEndTime:      0,
+			LockupDuration:     0,
+			EarlyUnlockPenalty: new(big.Int),
+		})
+	}
+
+	var owner common.Address
+	if num != 0 {
+		owner = validators[0].Address
+	}
+
+	genStore.SetMetadata(genesisstore.Metadata{
+		Validators:    validators,
+		FirstEpoch:    2,
+		Time:          time,
+		PrevEpochTime: time - inter.Timestamp(time.Time().Hour()),
+		ExtraData:     []byte("fake"),
+		DriverOwner:   owner,
+		TotalSupply:   totalSupply,
+	})
+	genStore.SetBlock(0, genesis.Block{
+		Time:        time - inter.Timestamp(time.Time().Minute()),
+		Atropos:     hash.Event{},
+		Txs:         types.Transactions{},
+		InternalTxs: types.Transactions{},
+		Root:        hash.Hash{},
+		Receipts:    []*types.ReceiptForStorage{},
+	})
+	// pre deploy NetworkInitializer
+	genStore.SetEvmAccount(netinit.ContractAddress, genesis.Account{
+		Code:    netinit.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// pre deploy NodeDriver
+	genStore.SetEvmAccount(driver.ContractAddress, genesis.Account{
+		Code:    driver.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// pre deploy NodeDriverAuth
+	genStore.SetEvmAccount(driverauth.ContractAddress, genesis.Account{
+		Code:    driverauth.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// pre deploy SFC
+	genStore.SetEvmAccount(sfc.ContractAddress, genesis.Account{
+		Code:    sfc.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// set non-zero code for pre-compiled contracts
+	genStore.SetEvmAccount(evmwriter.ContractAddress, genesis.Account{
+		Code:    []byte{0},
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+
+	return genStore
+}
+func FakeGenesisStore(num int, balance, stake *big.Int) *genesisstore.Store {
+	genStore := genesisstore.NewMemStore()
+	genStore.SetRules(galaxy.FakeNetRules())
+
+	validators := GetFakeValidators(num)
+
+	totalSupply := new(big.Int)
+	for _, val := range validators {
+		genStore.SetEvmAccount(val.Address, genesis.Account{
+			Code:    []byte{},
+			Balance: balance,
+			Nonce:   0,
+		})
+		genStore.SetDelegation(val.Address, val.ID, genesis.Delegation{
+			Stake:              stake,
+			Rewards:            new(big.Int),
+			LockedStake:        new(big.Int),
+			LockupFromEpoch:    0,
+			LockupEndTime:      0,
+			LockupDuration:     0,
+			EarlyUnlockPenalty: new(big.Int),
+		})
+		totalSupply.Add(totalSupply, balance)
+	}
+
+	var owner common.Address
+	if num != 0 {
+		owner = validators[0].Address
+	}
+
+	genStore.SetMetadata(genesisstore.Metadata{
+		Validators:    validators,
+		FirstEpoch:    2,
+		Time:          FakeGenesisTime,
+		PrevEpochTime: FakeGenesisTime - inter.Timestamp(time.Hour),
+		ExtraData:     []byte("fake"),
+		DriverOwner:   owner,
+		TotalSupply:   totalSupply,
+	})
+	genStore.SetBlock(0, genesis.Block{
+		Time:        FakeGenesisTime - inter.Timestamp(time.Minute),
+		Atropos:     hash.Event{},
+		Txs:         types.Transactions{},
+		InternalTxs: types.Transactions{},
+		Root:        hash.Hash{},
+		Receipts:    []*types.ReceiptForStorage{},
+	})
+	// pre deploy NetworkInitializer
+	genStore.SetEvmAccount(netinit.ContractAddress, genesis.Account{
+		Code:    netinit.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// pre deploy NodeDriver
+	genStore.SetEvmAccount(driver.ContractAddress, genesis.Account{
+		Code:    driver.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// pre deploy NodeDriverAuth
+	genStore.SetEvmAccount(driverauth.ContractAddress, genesis.Account{
+		Code:    driverauth.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// pre deploy SFC
+	genStore.SetEvmAccount(sfc.ContractAddress, genesis.Account{
+		Code:    sfc.GetContractBin(),
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+	// set non-zero code for pre-compiled contracts
+	genStore.SetEvmAccount(evmwriter.ContractAddress, genesis.Account{
+		Code:    []byte{0},
+		Balance: new(big.Int),
+		Nonce:   0,
+	})
+
+	return genStore
+}
+
+func GetFakeValidators(num int) gpos.Validators {
+	validators := make(gpos.Validators, 0, num)
+
+	for i := 1; i <= num; i++ {
+		key := FakeKey(i)
+		addr := crypto.PubkeyToAddress(key.PublicKey)
+		pubkeyraw := crypto.FromECDSAPub(&key.PublicKey)
+
+		validatorID := idx.ValidatorID(i)
+		validators = append(validators, gpos.Validator{
+			ID:      validatorID,
+			Address: addr,
+			PubKey: validatorpk.PubKey{
+				Raw:  pubkeyraw,
+				Type: validatorpk.Types.Secp256k1,
+			},
+			CreationTime:     FakeGenesisTime,
+			CreationEpoch:    0,
+			DeactivatedTime:  0,
+			DeactivatedEpoch: 0,
+			Status:           0,
+		})
+	}
+
+	return validators
+}
+
+type Genesis struct {
+	Nonce      uint64         `json:"nonce"`
+	Timestamp  uint64         `json:"timestamp"`
+	ExtraData  []byte         `json:"extraData"`
+	GasLimit   uint64         `json:"gasLimit"   gencodec:"required"`
+	Difficulty *big.Int       `json:"difficulty" gencodec:"required"`
+	Mixhash    common.Hash    `json:"mixHash"`
+	Coinbase   common.Address `json:"coinbase"`
+	Alloc      GenesisAlloc   `json:"alloc"      gencodec:"required"`
+
+	// These fields are used for consensus tests. Please don't use them
+	// in actual genesis blocks.
+	Number     uint64      `json:"number"`
+	GasUsed    uint64      `json:"gasUsed"`
+	ParentHash common.Hash `json:"parentHash"`
+	BaseFee    *big.Int    `json:"baseFeePerGas"`
+}
+
+type GenesisAlloc map[common.Address]GenesisAccount
+
+type GenesisAccount struct {
+	Code       []byte                      `json:"code,omitempty"`
+	Storage    map[common.Hash]common.Hash `json:"storage,omitempty"`
+	Balance    *big.Int                    `json:"balance" gencodec:"required"`
+	Nonce      uint64                      `json:"nonce,omitempty"`
+	PrivateKey []byte                      `json:"secretKey,omitempty"` // for tests
+}
